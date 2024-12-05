610bdf52b163819c7c54df3d14bcd0ce9f13e477
Peter-BC <bcxxboy@gmail.com>
2024-12-05 16:09:17 -0700

    init2

--- phpinfo.php
+++ phpinfo.php
@@ -1,2 +1,7 @@
 <?php
-phpinfo();
+$servername = getenv('DB_HOST') ?: '127.0.0.1';
+$username = getenv('DB_USERNAME') ?: 'myuser';
+$password = getenv('DB_PASSWORD') ?: 'mypassword';
+$dbname = getenv('DB_NAME') ?: 'mydatabase';
+
+var_dump($servername, $username, $password, $dbname);
