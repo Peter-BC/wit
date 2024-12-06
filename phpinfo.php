54ec6ad17ccbb6186a1165ae6eb8ea63d6451f6f
Peter-BC <bcxxboy@gmail.com>
2024-12-05 19:40:03 -0700

    init10

--- phpinfo.php
+++ phpinfo.php
@@ -1,11 +1,2 @@
 <?php
-$conn_str = getenv('MYSQLCONNSTR_localdb');
-parse_str(str_replace(';', '&', $conn_str), $db_params);
-
-$servername = $db_params['Server'];
-$dbname = $db_params['Database'];
-$username = $db_params['User'];
-$password = $db_params['Password'];
-
-
-var_dump($servername, $username, $password, $dbname);
+phpinfp();
