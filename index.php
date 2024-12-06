8869502bcf2ffc2b625554e09c888c457a5bc724
Peter-BC <bcxxboy@gmail.com>
2024-12-05 19:36:51 -0700

    init9

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 // Set the charset to UTF-8
-if (!$conn->set_charset("utf8")) {
+if (!$conn->set_charset("utf8mb4")) {
     die("Error loading character set utf8: " . $conn->error);
 }
 
